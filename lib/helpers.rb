e75c51580b1ef47acc37c0d7f73d8af6691fa53f
Peter Yates <peter.yates@graphia.co.uk>
2020-04-02 09:32:52 +0100

    Add .govuk-link to link_to if no class supplied

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,6 @@
+Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |f| require f }
+
 use_helper Nanoc::Helpers::Rendering
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Breadcrumbs
+use_helper Helpers::LinkHelper
