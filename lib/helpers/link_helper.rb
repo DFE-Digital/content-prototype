96fc7b4354e5aeca9101a8679882f655ee21ce35
Kyle Welsby <kyle@mekyle.com>
2020-04-02 10:49:37 +0100

    ✅ Content includes `govuk-link` class for links

--- lib/helpers/link_helper.rb
+++ lib/helpers/link_helper.rb
@@ -1,7 +1,7 @@
 module Helpers
   module LinkHelper
     def link_to(*args, **kwargs)
-      return super if kwargs.has_key?('class')
+      return super if kwargs.has_key?(:class)
 
       super(*args, **kwargs.merge(class: 'govuk-link'))
     end
