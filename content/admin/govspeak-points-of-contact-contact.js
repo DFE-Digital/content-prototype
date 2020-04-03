12ab19e0e4eeae580062a30da369bd431437e9ce
Kyle Welsby <kyle@mekyle.com>
2020-04-03 12:49:49 +0100

    ✨GovSpeak Points of Contact
    GovSpeak [Points of Contact](hhttps://github.com/alphagov/govspeak#points-of-contact)
    
    * Address
    * Contact

---
+++ content/admin/govspeak-points-of-contact-contact.js
@@ -0,0 +1,26 @@
+/* globals CMS:true */
+
+CMS.registerEditorComponent({
+  id: "points-of-contact-contact",
+  label: "Points of Contact - Contact",
+  fields: [{ name: "body", label: "Content", widget: "markdown" }],
+  pattern: /^\$C\n?([\w|\W|\n]*)\$C$/,
+  fromBlock: function (match) {
+    return {
+      body: match[1],
+    };
+  },
+  toBlock: function (obj) {
+    return ["$C", obj.body, "$C"].join("\n");
+  },
+  toPreview: function (obj) {
+    var str = [
+      '<div class="contact">',
+      "  <p>",
+      obj.body.replace(/\n/g, "<br>"),
+      "  </p>",
+      "</div>",
+    ].join("\n");
+    return str;
+  },
+});
