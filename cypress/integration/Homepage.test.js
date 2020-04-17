e58925f9fe0374e0331cece19dea9f8e2d56327c
Kyle Welsby <kyle@mekyle.com>
2020-04-17 10:39:48 +0100

    ♻️ Refactor Cypress tests to isolate pages

--- cypress/integration/Homepage.test.js
+++ cypress/integration/Homepage.test.js
@@ -1,9 +1,11 @@
-describe("HomePage", () => {
+describe("Landing Page", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.shouldHaveLargeHeading();
+    cy.shouldHaveFooter();
   });
+
   it("Shows the initial pages", () => {
-    cy.shouldHaveHeading();
     cy.get(".govuk-grid-column-two-thirds")
       .should("exist")
       .should((el) => {
@@ -13,20 +15,36 @@ describe("HomePage", () => {
     cy.get(
       ".govuk-grid-column-two-thirds > :nth-child(1) > .govuk-link"
     ).should("exist");
-    cy.get(".govuk-footer").should(
-      "contain.text",
-      "All content is available under the Open Government Licence v3.0, except where otherwise stated"
-    );
   });
-});
 
-describe("Teaching during coronavirus (COVID-19) hyperlink", () => {
-  cy.visit("/");
-  it("Checks the Teaching during coronavirus (COVID-19) hyperlink works", () => {
+  it('Checks the "Teaching during coronavirus" hyperlink works', () => {
     cy.get(".govuk-grid-column-two-thirds .govuk-link")
-      .should("have.attr", "href")
-      .should("equal", "/teachers/")
+      .eq(0)
+      .should((el) => {
+        expect(el).to.contain.text("Teaching during coronavirus (COVID-19)");
+        expect(el).to.have.attr("href");
+        expect(el.attr("href")).to.equal("/teachers/");
+      })
       .click();
     cy.location("pathname").should("equal", "/teachers/");
+  });
+
+  it('Checks the "Supporting your children\'s education" hyperlink works', () => {
+    cy.get(".govuk-grid-column-two-thirds .govuk-link")
+      .eq(1)
+      .should((el) => {
+        expect(el).to.contain.text(
+          "Supporting your children's education during coronavirus (COVID-19)"
+        );
+        expect(el).to.have.attr("href");
+        expect(el.attr("href")).to.equal(
+          "/supporting-your-childrens-education-during-coronavirus/"
+        );
+      })
+      .click();
+    cy.location("pathname").should(
+      "equal",
+      "/supporting-your-childrens-education-during-coronavirus/"
+    );
   });
 });
