c092a6005a25bc495e2eb49c500add1aa7174041
Sushant Kumar <Sushant.kumar@QUALITEST.LOCAL>
2020-04-23 13:25:07 +0100

    Add test for Help children with SEND continue their education during coronavirus page

---
+++ cypress/integration/Help-children-with-send-learn-at-home-during-coronavirus.test.js
@@ -0,0 +1,23 @@
+describe("Help children with SEND continue their education during coronavirus (COVID-19) page", () => {
+  beforeEach(() => {
+    cy.visit(
+      "/supporting-your-childrens-education-during-coronavirus/help-children-with-send-learn-at-home-during-coronavirus/"
+    );
+    cy.shouldHaveHeaderAndFooter();
+    cy.shouldHaveBreadcrumbs();
+  });
+
+  it("Shows the Help children with SEND continue their education during coronavirus (COVID-19) Page", () => {
+    cy.shouldHaveHeading(
+      "Help children with SEND continue their education during coronavirus (COVID-19)"
+    );
+  });
+
+  it('Has "Previous" pagination', () => {
+    cy.shouldHavePagination(
+      "/supporting-your-childrens-education-during-coronavirus/help-primary-school-children-learn-at-home-during-coronavirus/",
+      "Help primary school children continue their education during coronavirus (COVID-19)",
+      "Previous"
+    );
+  });
+});
