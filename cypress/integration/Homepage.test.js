29221925462675ee8fe73a8c849f39ad16aa4456
Sushant Kumar <Sushant.kumar@QUALITEST.LOCAL>
2020-04-28 12:26:11 +0100

    update test scripts for home page

--- cypress/integration/Homepage.test.js
+++ cypress/integration/Homepage.test.js
@@ -12,38 +12,49 @@ describe("Landing Page", () => {
         expect(el.find("a")).to.have.class("govuk-link");
       });
     cy.get(".submasthead__content").should("exist");
-    cy.get(
-      ".govuk-grid-column-two-thirds > :nth-child(1) > .govuk-link"
+    cy.get(".govuk-accordion__open-all").should("exist");
+    cy.get(".govuk-accordion__open-all").click();
+    cy.get(".govuk-grid-column-two-thirds > :nth-child(1)").should("exist");
+    cy.contains("School curriculum and teaching").should("exist");
+    cy.contains("Pupil wellbeing and safety").should("exist");
+    cy.contains(
+      "Closures, exams and managing a school or early years setting"
     ).should("exist");
+    cy.contains("Funding and support for education providers").should("exist");
+    cy.contains("Student accommodation, travel and financial support").should(
+      "exist"
+    );
+    cy.contains("Further and higher education and vocational training").should(
+      "exist"
+    );
   });
 
-  it('Checks the "Teaching during coronavirus" hyperlink works', () => {
+  it('Checks the "Remote education during coronavirus" hyperlink works', () => {
     cy.get(".govuk-grid-column-two-thirds .govuk-link")
-      .eq(0)
+      .eq(3)
       .should((el) => {
-        expect(el).to.contain.text(
-          "Remote education during coronavirus (COVID-19)"
-        );
+        expect(el).to.contain.text("Remote education during coronavirus");
         expect(el).to.have.attr("href");
         expect(el.attr("href")).to.equal("/teachers/");
       })
-      .click();
+      .click({ force: true });
     cy.location("pathname").should("equal", "/teachers/");
   });
 
   it('Checks the "Supporting your children\'s education" hyperlink works', () => {
+    cy.get("#accordion-default-heading-1").click();
     cy.get(".govuk-grid-column-two-thirds .govuk-link")
       .eq(1)
       .should((el) => {
         expect(el).to.contain.text(
-          "Supporting your children's education during coronavirus (COVID-19)"
+          "Supporting your children's education during coronavirus"
         );
         expect(el).to.have.attr("href");
         expect(el.attr("href")).to.equal(
           "/supporting-your-childrens-education-during-coronavirus/"
         );
       })
-      .click();
+      .click({ force: true });
     cy.location("pathname").should(
       "equal",
       "/supporting-your-childrens-education-during-coronavirus/"
